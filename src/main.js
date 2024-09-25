525320d96af37d27ef7b07b1579c987bf8f35f4e
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-09-25 17:24:20 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,8 +9,20 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(Flip,ScrollTrigger);
 
 
+const originalContainer = document.querySelector('.hero-elements_wrapper');
+const newContainer = document.querySelector('.main-content_earth-container');
+const img = document.querySelector('.hero-elements_content');
 
 document.querySelector('[home-hero="button"]').addEventListener('click',()=>{
 
+    const state = Flip.getState(img);
+
+    newContainer.appendChild(img);
+    
+    Flip.from(state, {
+        duration: 1,
+        ease: "power1.inOut",
+        scale: true
+    });
 
 });
