ab7a8c8595f7e789dcdd4d460ca130d3399a8882
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-11-04 15:11:12 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -461,6 +461,31 @@ function homepageJS(){
 
 
 
+function navLogoFlip() {
+  const originalNavContainer = document.querySelector('.logo-wrapper');
+  const newNavContainer = document.querySelector('.nav-bar_logo-container');
+  const navLogo = document.querySelector('.logo.is-hero');
+  gsap.set('.logo.is-nav', {display: 'none'});
+  
+  const navState = Flip.getState(navLogo);
+  ScrollTrigger.create({
+    trigger: '[data-nav="trigger"]',
+    start: 'top 80%',
+    end: 'top 20%',
+    onEnter: () => {
+      newNavContainer.appendChild(navLogo);
+      Flip.from(navState, { duration: 0.8, scale: true });
+      
+    },
+    onLeaveBack: () => {
+      const reverseState = Flip.getState(navLogo);
+      originalNavContainer.appendChild(navLogo);
+      Flip.from(reverseState, { duration: 0.8, scale: true });
+    }
+  });
+}
+
+
 document.querySelector('[data-gsap="enter"]').addEventListener('click', function() {
   loader();
   websiteLoadedAlready =  true;
@@ -542,6 +567,7 @@ barba.init({
         homepageJS();
         loaderInit();
         portfolioPopup();
+        navLogoFlip()
       },
       afterEnter() {
         ScrollTrigger.refresh(true);
