fbf9b99894faeb92b4d7c54f8631aa6d49389f12
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-11-04 17:15:49 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -432,27 +432,7 @@ function homepageJS(){
 
 
 function navLogoFlip() {
-  const originalNavContainer = document.querySelector('.logo-wrapper');
-  const newNavContainer = document.querySelector('.nav-bar_logo-container');
-  const navLogo = document.querySelector('.logo.is-hero');
-  gsap.set('.logo.is-nav', {display: 'none'});
-  
-  const navState = Flip.getState(navLogo);
-  ScrollTrigger.create({
-    trigger: '[data-nav="trigger"]',
-    start: 'top 80%',
-    end: 'top 20%',
-    onEnter: () => {
-      newNavContainer.appendChild(navLogo);
-      Flip.from(navState, { duration: 0.8, scale: true });
-      
-    },
-    onLeaveBack: () => {
-      const reverseState = Flip.getState(navLogo);
-      originalNavContainer.appendChild(navLogo);
-      Flip.from(reverseState, { duration: 0.8, scale: true });
-    }
-  });
+  console.log("flip was here")
 }
 
 
