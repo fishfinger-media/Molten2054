177429489227a41f7997284745fe0e8ad8246a63
Rahuli20 <111457292+Rahuli20@users.noreply.github.com>
2024-11-04 15:27:57 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -131,29 +131,6 @@ document.querySelector('.nav-bar_link.is-music').addEventListener('click', funct
   toggleMusic()
 });
 
-function navLogoFlip() {
-  const originalNavContainer = document.querySelector('.logo-wrapper');
-  const newNavContainer = document.querySelector('.nav-bar_logo-container');
-  const navLogo = document.querySelector('.logo.is-hero');
-  gsap.set('.logo.is-nav', {display: 'none'});
-  
-  const navState = Flip.getState(navLogo);
-  ScrollTrigger.create({
-    trigger: '[data-nav="trigger"]',
-    start: 'top 80%',
-    end: 'top 20%',
-    onEnter: () => {
-      newNavContainer.appendChild(navLogo);
-      Flip.from(navState, { duration: 0.8, scale: true });
-      
-    },
-    onLeaveBack: () => {
-      const reverseState = Flip.getState(navLogo);
-      originalNavContainer.appendChild(navLogo);
-      Flip.from(reverseState, { duration: 0.8, scale: true });
-    }
-  });
-}
 
 function loaderInit(){
   gsap.set('.footer', { opacity: 0 });
